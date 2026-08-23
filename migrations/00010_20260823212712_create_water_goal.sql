09b400b250660e1c127bcc209e36c5652f23d115
steven-dev8 <stevenndev8@gmail.com>
2026-08-23 18:35:51 -0300

    feat(migrations): recreate water goal table

--- migrations/00010_20260823212712_create_water_goal.sql
+++ migrations/00010_20260823212712_create_water_goal.sql
@@ -1,13 +1,12 @@
 -- +goose Up
 CREATE TABLE water_goal (
-    id BIGSERIAL PRIMARY KEY,
-
+    user_id BIGINT PRIMARY KEY
+        REFERENCES users(id)
+        ON DELETE CASCADE,
     goal_amount_ml INTEGER NOT NULL
         CHECK (goal_amount_ml > 0),
-
     created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
-
-    updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
 );
 
 -- +goose Down
